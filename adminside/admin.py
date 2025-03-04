f29d9d393956983cc8fcf9e5f1263cd462c4af41
Henil <henil00008@gmail.com>
2025-03-04 19:02:47 +0530

    saving in main branch

--- adminside/admin.py
+++ adminside/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Inventory,Branch,Purchase
 
 # Register your models here.
+class BranchAdmin(admin.ModelAdmin):
+  list_display = ("id", "location", "area","manager_id","phone_no","status")
+  
+admin.site.register(Branch, BranchAdmin)
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+  list_display = ("food_item_id", "food_item", "cost_price","supplier_id","purchased_date","payment_status")
+  
+admin.site.register(Purchase, PurchaseAdmin)
+
+class InventoryAdmin(admin.ModelAdmin):
+  list_display = ("food_item_id", "image", "food_item_name","category","description","quantity","branch","sell_price","cost_price","mfg_date","exp_date")
+  
+admin.site.register(Inventory, InventoryAdmin)
--- adminside/models.py
+++ adminside/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class Branch(models.Model):
+    id=models.IntegerField(primary_key=True)
+    location=models.CharField(max_length=50)
+    area=models.CharField(max_length=50)
+    manager_id=models.IntegerField(null=False)
+    phone_no=PhoneNumberField()
+    status=models.CharField(max_length=10)
+
+class Purchase(models.Model):
+    food_item_id=models.IntegerField(primary_key=True)
+    food_item=models.CharField(max_length=50)
+    cost_price=models.IntegerField(null=False)
+    supplier_id=models.IntegerField(null=False)
+    purchased_date=models.DateField()
+    payment_status=models.CharField(max_length=10)
+
+class Inventory(models.Model):
+    food_item_id=models.IntegerField(primary_key=True)
+    image=models.ImageField(blank=True)
+    food_item_name=models.CharField(max_length=20)
+    category=models.CharField(max_length=20)
+    description=models.TextField(max_length=100)
+    quantity=models.IntegerField(null=True)
+    branch=models.CharField(max_length=20)
+    sell_price=models.IntegerField(null=False)
+    cost_price=models.IntegerField(null=False)
+    mfg_date=models.DateField()
+    exp_date=models.DateField()
